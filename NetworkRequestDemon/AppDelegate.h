dfedc111278d46bf669ed005a1edb3ed25ed4c85
wanc <chao>
2017-02-21 16:58:54 +0800

    Initial Commit

---
+++ NetworkRequestDemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkRequestDemon
+//
+//  Created by wanc on 2017/2/21.
+//  Copyright © 2017年 zdsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
